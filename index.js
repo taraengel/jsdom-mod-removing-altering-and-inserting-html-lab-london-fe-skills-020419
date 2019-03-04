1019cec977bebde15e5e64d3a40bc39323f6ac81
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-04 19:28:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ main.remove();
 
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement('h1');
-document.body.appendChild(newHeader)
-newHeader.innerHTML = "Tara is the champion!"
-newHeader.id = 'victory'
+newHeader.innerHTML = "Tara is the champion!";
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
