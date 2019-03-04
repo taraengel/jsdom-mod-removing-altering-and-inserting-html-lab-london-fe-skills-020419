1ce5e3dc65c5b5ee96dabbbf09653952b2ca145b
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-04 19:33:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove();
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+const newHeader = document.createElement('h1');
+newHeader.innerText = "Tara is the champion";
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
